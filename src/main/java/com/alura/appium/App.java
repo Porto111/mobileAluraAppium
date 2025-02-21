3a1709e7ce8e4c35382b2b9d7aa0eeb5c630a982
Porto111 <portoengenheiro@gmail.com>
2025-02-21 18:57:29 -0300

    Configurando a instalação do app que será testado

--- src/main/java/com/alura/appium/App.java
+++ src/main/java/com/alura/appium/App.java
@@ -1,7 +1,22 @@
 package com.alura.appium;
 
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.remote.MobilePlatform;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws MalformedURLException {
+
+        File apk = new File("C:\\Users\\Guilherme\\Desktop\\Projetos QA\\mobileAluraAppium\\src\\main\\resources\\alura_esporte.apk");
+        DesiredCapabilities configuracoes = new DesiredCapabilities();
+        configuracoes.setCapability(MobilePlatform.ANDROID, apk.getAbsolutePath());
+        configuracoes.setCapability(MobilePlatform.ANDROID, "uiAutomator2");
+
+        URL urlConexao = new URL("http://127.0.0.1:4723/wd/hub");
+        AppiumDriver driver = new AppiumDriver(urlConexao, configuracoes);
     }
 }
